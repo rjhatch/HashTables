25a0f7dff4d6a9a2b4b576353ffcb40fc86fea5e
Richie <richdev378@gmail.com>
2021-10-05 15:02:26 -0600

    Add project files.

---
+++ HashTablesTests/HashTablesTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTablesTests
+{
+    [TestClass]
+    public class HashTablesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
